c41471ea1355b5a8f41ecfe055abd8b11e46f67c
santhu81474 <santhoshkuar18@gmail.com>
2026-04-13 21:57:09 +0530

    Update vite.config.js
    hfsh

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig({
   plugins: [
     react(),
-    cloudflare()
   ],
 
   server: {
@@ -18,5 +16,13 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   }
 })
